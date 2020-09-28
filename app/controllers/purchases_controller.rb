364b6a5a5e6cf5840949b308b026d567a00abd9b
aabboud1 <abboudahmed1@gmail.com>
2020-09-28 12:01:19 -0400

    working on purchase form

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,20 @@
 class PurchasesController < ApplicationController
     def new
-        # @inventory = Inventory.find(params[:format])
-        # @purchase = Purchase.new(store_id: @inventory[:store_id], product_id: @inventory[:product_id])
-        @purchase = Purchase.new
+        if params[:format]
+            @inventory = Inventory.find(params[:format])
+            @purchase = Purchase.new(store_id: @inventory[:store_id], product_id: @inventory[:product_id])
+        else
+            @purchase = Purchase.new
+        end
     end
     
     def create
         #throwing error, need to make strong params and pass them possibly take off inventroy and inven.find
-        inventory = Inventory.find_by(params[:purchase][:store_id], params[:purchase][:product_id].quantity)
-        if inventory.quantity > 0
-            @purchase = Purchase.create(
-                customer_id: params[:purchase][:customer_id],
-                product_id: params[:purchase][:product_id],
-                store_id: params[:purchase][:store_id]
-            )
+        
+        @purchase = Purchase.new
+        inventory = Inventory.find_by(params[:purchase][:store_id], params[:purchase][:product_id])
+        if @purchase.save && inventory.quantity > 0 
+            @purchase = Purchase.save(purchase_params)
             Inventory.find_by(params[:purchase][:store_id], params[:purchase][:product_id].quantity).quantity -= 1
         else
             #error message stuff here
@@ -23,5 +24,9 @@ class PurchasesController < ApplicationController
 
 
     private
+
+    def purchase_params
+        params.require(:purchase).permit(:customer_id, :product_id, :store_id)
+    end
     
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,7 +3,7 @@ class Purchase < ApplicationRecord
     belongs_to :product
     belongs_to :store
 
-    validates :product_id, presence: true
-    validates :customer_id, presence: true
-    validates :store_id, presence: true
+    # validates :product_id, presence: true
+    # validates :customer_id, presence: true
+    # validates :store_id, presence: true
 end
