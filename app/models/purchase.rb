6ad5545305a75d50f2654512453e7f35cfa250af
conrad_buys <conradbuys11@gmail.com>
2020-09-28 17:50:54 -0400

    making created_at look readable for purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,4 +6,40 @@ class Purchase < ApplicationRecord
     # validates :product_id, presence: true
     # validates :customer_id, presence: true
     # validates :store_id, presence: true
+
+    def print_time
+        "#{find_month_name(self.created_at.month)} #{self.created_at.day}, #{self.created_at.year}"
+    end
+
+    private
+    def find_month_name(month)
+        case month
+            when 1
+                "January"
+            when 2
+                "February"
+            when 3
+                "March"
+            when 4
+                "April"
+            when 5
+                "May"
+            when 6
+                "June"
+            when 7
+                "July"
+            when 8
+                "August"
+            when 9
+                "September"
+            when 10
+                "October"
+            when 11
+                "November"
+            when 12
+                "December"
+            else
+                false
+        end
+    end
 end
