807f42f2d75ac48787dcb5e903cf5453a264627a
conrad_buys <conradbuys11@gmail.com>
2020-09-30 16:18:16 -0400

    Made new inventory update existing inventory if applicable. Made new purchase say the store you're purchasing from.

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -3,17 +3,28 @@ class InventoriesController < ApplicationController
     before_action :manager_authorized
     
     def new
-        @inventory = Inventory.new
+        if params[:format]
+            @store = Store.find(params[:format])
+            @inventory = Inventory.new(store_id: @store.id)
+        else
+            #MAKE IT THROW AN ERROR IF WE DON'T INCLUDE A STORE
+            @inventory = Inventory.new
+        end
     end
 
     def create
         @inventory = Inventory.new(strong_params)
-        # @existing_inventory = Inventory.find_by(store_id: params[:inventory][:store_id], product_id: params[:inventory][:product_id])
+        @existing_inventory = Inventory.find_by(store_id: params[:inventory][:store_id], product_id: params[:inventory][:product_id])
+        #if we already have an inventory with our current inventory's params
 
-        if @inventory.valid? #&& !!@existing_inventory[:quantity]
-            @inventory.save
-            # @existing_inventory.increase_quantity(@inventory.quantity)
-            redirect_to product_path(@inventory.product_id)
+        if @inventory.valid? #if we made a valid inventory
+            if @existing_inventory #if our check for existing inventory came up with something
+                @existing_inventory.increase_quantity(@inventory.quantity) #instead of making a new inventory item, we'll add the quantity to our existing inventory item
+                redirect_to product_path(@existing_inventory.product_id)
+            else
+                @inventory.save
+                redirect_to product_path(@inventory.product_id)
+            end
         else
             #error message stuff here
             #need to make custom error messages for !!@inventory[:quantity]
