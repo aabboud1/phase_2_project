bda70d71665450c937c954a622ba4a1981ea4a8c
conrad_buys <conradbuys11@gmail.com>
2020-09-30 14:20:19 -0400

    added sitewide header saying name, whether they're a manager, homepage, and login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
     #see 24-authorization for more info
 
-    helper_method :current_user, :current_user_name, :logged_in?
+    helper_method :current_user, :current_user_name, :logged_in?, :manager?
 
     def current_user
         Customer.find_by(id: session[:customer_id])
@@ -24,9 +24,14 @@ class ApplicationController < ActionController::Base
     end
 
     def manager_authorized
-        if !logged_in? || current_user.manager_of_store_id == nil
+        if !manager? #if we aren't a manager
             flash[:message] = "You must be a manager to access this."
             redirect_to login_path
         end
+    end
+
+    def manager?
+        logged_in? && current_user.manager_of_store_id != nil
+        #will return true if we are logged in, & we are a manager of a store
     end
 end
